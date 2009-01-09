469922be74d2a90487c4c902c48058c5cb0be916
Daniel Lucraft <dan@fluentradical.com>
2009-01-09 09:08:36 +0000

    Added table of contents

--- lib/features_report/generator.rb
+++ lib/features_report/generator.rb
@@ -3,11 +3,19 @@ module FeaturesReport
   class Generator
     def self.generate(reader)
       Prawn::Document.generate("features.pdf") do
+        text "Contents", :size => 24
+        
+        reader.features.each_with_index do |feature, i|
+          text "#{i+1}. #{feature.title}"
+        end
+
+        start_new_page
+
         reader.features.each do |feature| 
-          text feature.title, :size => 32
+          text feature.title, :size => 24
 
           feature.scenarios.each do |scenario|
-            text scenario.name, :size => 24
+            text scenario.name, :size => 16
 
             scenario.steps.each do |step|
               text step.name
